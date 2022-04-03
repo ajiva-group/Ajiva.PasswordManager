9145acffb716aa6f00a9234ee3ac723a78f691d4
xuri02 <t0bl4s.durst@gmail.com>
2022-04-03 19:26:48 +0200

    Main App Layout

---
+++ Ajiva.PasswordManager.Ui.Maui/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Ajiva.PasswordManager.Ui.Maui.Pages;
+using Ajiva.PasswordManager.Ui.Maui.Static;
+using VaultManager.Crypto;
+
+namespace Ajiva.PasswordManager.Ui.Maui;
+
+public partial class App : Application
+{
+    Page _realPage;
+
+    public App()
+    {
+        InitializeComponent();
+
+        if (DeviceInfo.Idiom == DeviceIdiom.Phone)
+            Shell.Current.CurrentItem = PhoneTabs;
+
+        _realPage = MainPage;
+
+        var mainPage = new MainPage();
+        mainPage.OnLogin += MainPage_OnLogin;
+        MainPage = mainPage;
+
+        Routing.RegisterRoute("settings", typeof(SettingsPage));
+    }
+
+    private void MainPage_OnLogin(object sender, string e)
+    {
+        StaticData.Load(e);
+
+        MainPage = _realPage;
+    }
+}
