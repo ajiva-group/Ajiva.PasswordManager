1dd1995dd25737ebd54c5f1e72c40e5c5e286045
xuri02 <t0bl4s.durst@gmail.com>
2022-04-03 19:26:23 +0200

    Static Data for Global access

---
+++ Ajiva.PasswordManager.Ui.Maui/Static/StaticData.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VaultManager;
+using VaultManager.Crypto;
+using VaultManager.Providers;
+
+namespace Ajiva.PasswordManager.Ui.Maui.Static
+{
+    internal class StaticData
+    {
+        public static VaultManager.VaultManager Vault { get; set; }
+
+        static StaticData()
+        {
+            var store = IsolatedStorageFile.GetUserStoreForApplication();
+        }
+
+        public static void Load(string password)
+        {
+            IEncryptionProvider encryptionProvider = new EncryptionManager();
+            IStorageProvider storageProvider = new MauiStorageProvider();
+            IValueSerializationManager serializationManager = new ValueSerializationManager();
+            IKeyManager keyManager = new KeyManager(encryptionProvider, storageProvider, "default", password);
+
+            Vault = new VaultManager.VaultManager("default", encryptionProvider, keyManager, storageProvider, serializationManager);
+
+
+            try
+            {
+                Vault.LoadVault();
+            }
+            catch (VaultNotFound e)
+            {
+                Vault.CreateVault();
+            }
+        }
+    }
+}
