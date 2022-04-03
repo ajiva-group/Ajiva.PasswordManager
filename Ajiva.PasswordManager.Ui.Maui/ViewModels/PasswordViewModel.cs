e307521d76d09ae934ff50efd1e20bbcbd7b141e
xuri02 <t0bl4s.durst@gmail.com>
2022-04-03 19:27:43 +0200

    Added View Models

---
+++ Ajiva.PasswordManager.Ui.Maui/ViewModels/PasswordViewModel.cs
@@ -0,0 +1,59 @@
+﻿using Ajiva.PasswordManager.Ui.Maui.Static;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Ajiva.PasswordManager.Ui.Maui.ViewModels;
+
+public class PasswordViewModel : INotifyPropertyChanged
+{
+
+
+    private ObservableCollection<PasswordVm> passwords;
+    public ObservableCollection<PasswordVm> Passwords
+    {
+        get
+        {
+            return passwords;
+        }
+
+        set
+        {
+            passwords = value;
+            OnPropertyChanged();
+        }
+    }
+
+    async void Fetch()
+    {
+        var vault = StaticData.Vault.Vault;
+        var passwords = vault.Passwords.Select(x => new PasswordVm(x.Value, vault)).ToList();
+
+        UpdatePasswords(passwords);
+
+        OnPropertyChanged(nameof(Passwords));
+    }
+
+    private void UpdatePasswords(IEnumerable<PasswordVm> locations)
+    {
+        passwords = new ObservableCollection<PasswordVm>();
+        for (int i = locations.Count() - 1; i >= 0; i--)
+        {
+            passwords.Add(locations.ElementAt(i));
+        }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        PropertyChangedEventHandler handler = PropertyChanged;
+        if (handler != null)
+            handler(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    public PasswordViewModel()
+    {
+        Fetch();
+    }
+}
---
+++ Ajiva.PasswordManager.Ui.Maui/ViewModels/PasswordVm.cs
@@ -0,0 +1,123 @@
+﻿using VaultManager;
+
+namespace Ajiva.PasswordManager.Ui.Maui.ViewModels;
+
+public class PasswordVm
+{
+    public PasswordVm(PasswordEntry argValue, Vault vault)
+    {
+        Id = argValue.Id;
+        Username = argValue.Username;
+        Password = argValue.Password;
+        Description = argValue.Description;
+        Notes = argValue.Notes;
+
+        WebSide = new WebSideVm(argValue.WebSide);
+        TwoFactor =argValue.TwoFactor is null ? null : new TwoFactorVm(vault.TwoFactors[argValue.TwoFactor.Value], this);
+        Tags = argValue.Tags?.Select(x => new TagVm(x)).ToList() ?? new List<TagVm>();
+    }
+
+    public Guid Id { get; set; }
+
+    public string Description { get; set; }
+
+    public WebSideVm WebSide { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string Notes { get; set; }
+
+    public TwoFactorVm? TwoFactor { get; set; }
+    public List<TagVm> Tags { get; set; }
+}
+public class TwoFactorVm
+{
+    public TwoFactorVm(TwoFactorEntry twoFactor, PasswordVm credential)
+    {
+        Id = twoFactor.Id;
+        Description = twoFactor.Description;
+        Type = twoFactor.Type;
+        Secret = twoFactor.Secret;
+        Credential = credential;
+        Tags = twoFactor.Tags?.Select(x => new TagVm(x)).ToList() ?? new List<TagVm>();
+    }
+    public Guid Id { get; set; }
+    public string Description { get; set; }
+
+    public string Type { get; set; }
+    public string Secret { get; set; }
+
+    public PasswordVm Credential { get; set; }
+
+    public List<TagVm> Tags { get; set; }
+}
+public class IdentityVm
+{
+    public Guid Id { get; set; }
+    public string Description { get; set; }
+
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    public string Address { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string Zip { get; set; }
+    public string Country { get; set; }
+
+    public string Notes { get; set; }
+    public List<TagVm> Tags { get; set; }
+}
+public class WebSideVm
+{
+    public WebSideVm(WebSideEntry webSide)
+    {
+        Id = webSide.Id;
+        Description = webSide.Description;
+        Domain = webSide.Domain;
+        Settings = new WebSideSettingsVm(webSide.Settings);
+        FavIcon = new UriBuilder(webSide.Domain) { Path = "/favicon.ico" }.Uri;
+        Tags = webSide.Tags?.Select(x => new TagVm(x)).ToList() ?? new List<TagVm>();
+    }
+
+    public Guid Id { get; set; }
+    public string Description { get; set; }
+    public Uri FavIcon { get; set; }
+    public Uri Domain { get; set; }
+    public List<PasswordVm> Passwords { get; set; }
+    public WebSideSettingsVm? Settings { get; set; }
+
+    public List<TagVm> Tags { get; set; }
+}
+public class WebSideSettingsVm
+{
+    public WebSideSettingsVm(WebSideSettings settings)
+    {
+        
+    }
+}
+public class FileNoteVm
+{
+    public Guid Id { get; set; }
+    public string Description { get; set; }
+
+    public string FileName { get; set; }
+    public string FilePath { get; set; }
+    public string Notes { get; set; }
+    public byte[] FileData { get; set; }
+
+    public List<TagVm> Tags { get; set; }
+}
+public class TagVm
+{
+    public TagVm(TagEntry tagEntry)
+    {
+        Id = tagEntry.Id;
+        Description = tagEntry.Description;
+        Name = tagEntry.Name;
+    }
+
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
