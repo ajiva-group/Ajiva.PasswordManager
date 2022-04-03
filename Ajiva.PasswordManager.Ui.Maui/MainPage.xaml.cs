0bdb1b7565382a121c5787a7a81dd6c51d760a24
xuri02 <t0bl4s.durst@gmail.com>
2022-04-03 19:25:53 +0200

    Login

---
+++ Ajiva.PasswordManager.Ui.Maui/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Ajiva.PasswordManager.Ui.Maui.Static;
+using VaultManager;
+using VaultManager.Crypto;
+
+namespace Ajiva.PasswordManager.Ui.Maui
+{
+    public partial class MainPage : ContentPage
+    {
+        public event EventHandler<string> OnLogin;
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+            //CounterLabel.Text = $"Current count: {count}";
+
+            //SemanticScreenReader.Announce(CounterLabel.Text);
+        }
+
+        private void OnUnlockClicked(object sender, EventArgs e)
+        {
+            OnLogin?.Invoke(this, PasswordInput.Text);
+        }
+    }
+}
